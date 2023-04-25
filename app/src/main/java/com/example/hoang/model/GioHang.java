8fb19c802949428931523b2c907dfa3df8e8477b
tronghoang89 <tronghoangn999@gmail.com>
2023-04-25 10:54:33 +0700

    add new project

---
+++ app/src/main/java/com/example/hoang/model/GioHang.java
@@ -0,0 +1,52 @@
+package com.example.hoang.model;
+
+public class GioHang {
+    int idsp;
+    String tensp;
+    long giasp;
+    String hinhsp;
+    int soluong;
+
+    public GioHang() {
+    }
+
+    public int getIdsp() {
+        return idsp;
+    }
+
+    public void setIdsp(int idsp) {
+        this.idsp = idsp;
+    }
+
+    public String getTensp() {
+        return tensp;
+    }
+
+    public void setTensp(String tensp) {
+        this.tensp = tensp;
+    }
+
+    public long getGiasp(long gia) {
+        return giasp;
+    }
+
+    public void setGiasp(long giasp) {
+        this.giasp = giasp;
+    }
+
+    public String getHinhsp() {
+        return hinhsp;
+    }
+
+    public void setHinhsp(String hinhsp) {
+        this.hinhsp = hinhsp;
+    }
+
+    public int getSoluong() {
+        return this.soluong;
+    }
+
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
+}
---
+++ app/src/main/java/com/example/hoang/utils/Utils.java
@@ -0,0 +1,10 @@
+package com.example.hoang.utils;
+
+import com.example.hoang.model.GioHang;
+
+import java.util.List;
+
+public class Utils {
+    public static final String BASE_URL="http://192.168.81.1/banhang/";
+    public static List<GioHang>gioHang;
+}
